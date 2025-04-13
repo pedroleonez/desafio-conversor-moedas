379b00873ade4c65df322f857bcca54b2f42e3d8
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-12 22:08:53 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
